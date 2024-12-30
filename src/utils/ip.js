d0b0b531f3ff814928fab154fafd4e9d7319b683
NotKiwy <c3zYyyyx.offical@gmail.com>
2024-12-30 13:06:14 +0300

    Update ip.js

--- src/utils/ip.js
+++ src/utils/ip.js
@@ -1,1 +1,19 @@
+const getClientIP = (req) => {
+    return req.headers['x-forwarded-for']?.split(',')[0].trim() || 
+           req.connection.remoteAddress;
+};
 
+const isIPBlocked = async (ip) => {
+    try {
+        const blockedIP = await BlockedIP.findOne({ ip });
+        return !!blockedIP;
+    } catch (error) {
+        console.error('Error checking IP status:', error);
+        return false;
+    }
+};
+
+module.exports = {
+    getClientIP,
+    isIPBlocked
+};
